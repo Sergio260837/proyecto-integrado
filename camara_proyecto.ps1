5d41898960b8d699651d759a57a96ebdd3ad94b2
sergio <sergiosierrajaramillo@gmail.com>
2025-05-29 10:19:24 +0200

    Add files via upload

---
+++ camara_proyecto.ps1
@@ -0,0 +1,16 @@
+﻿# Rutas de origen y destino
+$origen = "C:\prueba_caras"
+$destino = "C:\caras_autorizadas"
+
+# Crear carpeta de destino si no existe
+if (!(Test-Path $destino)) {
+    New-Item -Path $destino -ItemType Directory
+}
+
+# Extensiones de imagen que se pueden llegar a mover
+$extensiones = @("*.jpg", "*.jpeg", "*.png", "*.bmp", "*.gif", "*.tiff")
+
+# Mover imagenes
+foreach ($ext in $extensiones) {
+    Move-Item -Path (Join-Path $origen $ext) -Destination $destino -Force -ErrorAction SilentlyContinue
+}
